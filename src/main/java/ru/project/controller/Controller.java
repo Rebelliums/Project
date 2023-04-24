1ce38add64955784d067506fdace03ecb44db7f6
Vlad <hramchenkovlad16@gmal.com>
2023-04-24 16:35:09 +0300

    Init project

---
+++ src/main/java/ru/project/controller/Controller.java
@@ -0,0 +1,18 @@
+package ru.project.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class Controller
+{
+
+    @PostMapping("/test")
+    public void test()
+    {
+
+    }
+
+}
---
+++ src/main/java/ru/project/service/CoreService.java
@@ -0,0 +1,12 @@
+package ru.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoreService
+{
+    public void test()
+    {
+        System.out.println("/tetetetet");
+    }
+}
